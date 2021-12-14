0fc0e91b8b1539f91a953d02d3e772c8e1e83a9b
kkn1125 <xpfjrnseks1@naver.com>
2021-12-14 17:03:06 +0900

    add link in ddd

--- pages/domain-driven-design.js
+++ pages/domain-driven-design.js
@@ -53,6 +53,10 @@ const domain_driven_design = {
             link: 'https://gnidoc.tistory.com/entry/%EB%8F%84%EB%A9%94%EC%9D%B8-%EC%A3%BC%EB%8F%84-%EC%84%A4%EA%B3%84DDD-Domain-Driven-Design-%EB%8F%84%EB%A9%94%EC%9D%B8-%EB%AA%A8%EB%8D%B8',
         },
         {
+            name: 'Incheol\'s TECH BLOG - 1장 도메인 모델 시작',
+            link: 'https://incheol-jung.gitbook.io/docs/study/ddd-start/1',
+        },
+        {
             name: 'joont92님 블로그 - [ddd] 도메인 모델',
             link: 'https://joont92.github.io/ddd/%EB%8F%84%EB%A9%94%EC%9D%B8-%EB%AA%A8%EB%8D%B8/',
         },
