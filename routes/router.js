5f3ec16f25ec9187cbf1c510cea18b985a8acddc
kkn1125 <xpfjrnseks1@naver.com>
2022-01-28 16:59:57 +0900

    전체 페이지 새로운 라우터로 교체 완료, 페이지 정리

--- routes/router.js
+++ routes/router.js
@@ -1,17 +1,17 @@
 'use strict';
 import {Router, Layout} from '../core/core.js'
 
-import Home from '../../views/pages/home.js'
-import Algorithm from '../../views/pages/algorithm.js'
-import Javascript from '../../views/pages/javascript.js'
-import Interview from '../../views/pages/interview.js'
-import Cs from '../../views/pages/cs.js'
-import About from '../../views/pages/about.js'
+import Home from '../views/pages/home.js'
+import Algorithm from '../views/pages/algorithm.js'
+import Javascript from '../views/pages/javascript.js'
+import Interview from '../views/pages/interview.js'
+import Cs from '../views/pages/cs.js'
+import About from '../views/pages/about.js'
 
-import nav from '../../views/common/nav.js'
-import footer from '../../views/common/footer.js'
-import sidebar from '../../views/common/sidebar.js'
-import dotted from '../../views/common/dotted.js'
+import nav from '../views/common/nav.js'
+import footer from '../views/common/footer.js'
+import sidebar from '../views/common/sidebar.js'
+import dotted from '../views/common/dotted.js'
 
 Router.setPage('home', Home);
 Router.setPage('algorithm', Algorithm);
