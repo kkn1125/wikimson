47498681e6e17070b41874ec1fdab30c59833f6d
kkn1125 <xpfjrnseks1@naver.com>
2022-03-09 13:22:50 +0900

    :bulb: post: 모니터 포스팅 작성 중 2022년 03월  9일 수 오후  1:22:41

--- views/pages/os/os.monitor.js
+++ views/pages/os/os.monitor.js
@@ -12,6 +12,16 @@ export default {
     content: [`
 # 모니터
 
+모니터는 세마포 이후의 고수준 개념의 동기화 도구이다. 세마포를 떠올려보면 \`acquire\`와 \`release\`가 있었고, value값에 대한 조건문으로 동기화를 관리했다.
+
+## 모니터의 구조
+
+공유자원과 공유자원에 접근하는 함수를 가진다. \`common variable\`이 있고, 여기에 접근하는 함수들이 있다.
+
+Queue를 2개 가지는데 하나는 배타동기(Mutual exclusion)를 위함이고, 다른 하나는 조건동기(Conditional Synchronization)를 위한 Queue이다.
+
+배타동기 큐는 common variable에 접근하는 메서드가 있을 때 하나의 스레드만 접근 가능하도록 대기 시키는 역할을 한다.
+
 ... 작성 중 ...
 `],
     ref: [
